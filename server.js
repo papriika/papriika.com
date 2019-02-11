aefd342a4f357d54d34d942e4bb35d34e20eac6d
Guy Lepage <guylepage3@gmail.com>
2019-02-11 08:15:46 -0500

    update to production path #14
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

--- server.js
+++ server.js
@@ -6,16 +6,16 @@ const app = express();
 //Static file declaration
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-//production mode
+//Production mode
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
   //
   app.get('*', (req, res) => {
-    res.sendfile(path.join(__dirname = 'client', 'build', 'index.html'));
+    res.sendfile(path.join(__dirname = 'client/public/index.html'));
   })
 }
 
-//build mode
+//Build mode
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/public/index.html'));
 })
