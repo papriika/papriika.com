d48d53c9b8a37355150566de60f7f595607f22bb
Guy Lepage <guylepage3@gmail.com>
2019-02-18 18:39:53 -0500

    added 404 page
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

---
+++ client/src/containers/NoMatch.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Header from '../components/Navbar';
+import Page404 from './Page404';
+
+class NoMatch extends Component {
+  render() {
+    return (
+      <div className="d-flex w-100 h-100 mx-auto flex-column">
+        <Header />
+        <div 
+          style={{
+            paddingTop: '56px'
+          }}>
+        </div>
+        <Page404 />
+      </div>
+    );
+  }
+}
+
+export default NoMatch;
