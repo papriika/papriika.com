b96b74ed856112731d022e85a3c3dafa20d218bb
Guy Lepage <guylepage3@gmail.com>
2019-03-07 15:55:20 -0500

    removed navbar css import
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import NavbarToggler from '../components/NavbarToggler';
 import logoWordmarkDark from './../img/papriika-logo-wordmark-1.svg';
-import '../css/Navbar.css';
 
 const Nav = styled.nav`
   padding-top: 0.15rem;
