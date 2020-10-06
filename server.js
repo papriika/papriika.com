abf549fc26dde880059e09ba433e94cd18a9496e
Guy Lepage <guylepage3@gmail.com>
2020-10-06 12:16:23 -0400

    update dependencies, set sslredirect to default
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const sslRedirect = require('heroku-ssl-redirect');
+const sslRedirect = require('heroku-ssl-redirect').default;
 const express = require('express');
 const morgan = require('morgan');
 const request = require('request');
