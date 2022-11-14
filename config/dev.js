0c72d5a713a0d8b0f2fc01ee74a3081344c45f32
Guy Lepage <guylepage3@gmail.com>
2022-11-13 21:21:03 -0500

    update to gitignore
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+// Hold application secrets and config for development environment
+module.exports = {
+  mailchimpAPI: '478c568459ab4be2e28f7d598f1aacc1-us4',
+  gmailMailer: 'general@papriika.com',
+  gmailSecret: 'DWRAkX[DWakj]bkge6YchzJkjaBHJ65KgJGa9'
+};
